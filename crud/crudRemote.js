acfb004eec76be4abd8fe9eca0f32ccf61a1ebd6
Bruna <baws@cin.ufpe.br>
2023-04-02 17:49:37 -0300

    Crud com crudcrud de um catálogo de itens de pokemon a venda

---
+++ crud/crudRemote.js
@@ -0,0 +1,150 @@
+const baseUrl = "https://crudcrud.com/api/76eefe477cbd49e3a4994efb1e08748a";
+
+function saveLocal(lista) {
+  localStorage.setItem("@itemstore", JSON.stringify(lista));
+}
+
+function getLocal() {
+  return JSON.parse(localStorage.getItem("@itemstore")) || [];
+}
+
+function saveRemote(item) {
+  return fetch(`${baseUrl}/item`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
+  });
+}
+
+function deleteRemote(id) {
+    return fetch(`${baseUrl}/item/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+}
+
+function updateRemote(id, newitem) {
+    return fetch(`${baseUrl}/item/${id}`, {
+        method: "PUT",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newitem),
+    });
+}
+
+async function getAll() {
+  const response = await fetch(`${baseUrl}/item`);
+  const data = await response.json();
+  return data;
+}
+
+async function onclickTrash(item) {
+    await deleteRemote(item);
+    const list = await getAll();
+    saveLocal(list)
+    createItemsList(list);
+};
+
+async function EditSubmit(id) {
+    console.log(id)
+    const nomeEl = document.getElementById("itemNameEdit");
+    const precoEl = document.getElementById("itemPriceEdit");
+    const nome = nomeEl.value;
+    const preco = precoEl.value;
+
+    if (nome === "") return;
+    if (preco === "") return;
+
+    const item = { name: nome, price: preco };
+
+    console.log(item);
+
+    await updateRemote(id, item);
+    const list = await getAll();
+    saveLocal(list)
+    createItemsList(list);
+}
+
+async function onclickEdit(id) {
+    const list = await getAll();
+    const ul = document.querySelector("ul");
+    ul.innerHTML = "";
+
+    console.log(id)
+    list.map((item) => {
+        if (item._id === id) {
+            ul.innerHTML += 
+            `<li>
+                <p><b>Edit item ${item.name}</b></p>
+                <div>
+                    <input id="itemNameEdit" type="text" placeholder="Item name" value=${item.name}>
+                <div>
+                <div>
+                    <input id="itemPriceEdit" type="number" placeholder="Item price" value=${item.price}> 
+                </div>
+                <button onclick='EditSubmit(${JSON.stringify(item._id)})'>Save</button>
+            </li>
+            
+            `
+        } else {
+            ul.innerHTML += 
+            `<li>
+                ${item.name} - R$${item.price}  
+                <button onclick='onclickTrash(${JSON.stringify(item._id)})'><i class="fa fa-trash-o"></i></button>
+                <button onclick='onclickEdit(${JSON.stringify(item._id)})'><i class="fa fa-edit"></i></button>
+            </li>`
+        }
+      });
+}
+
+function createItemsList(list) {
+
+  const ul = document.querySelector("ul");
+  ul.innerHTML = "";
+  list.map((item) => {
+    ul.innerHTML += 
+    `<li>
+        ${item.name} - R$${item.price}  
+        <button onclick='onclickTrash(${JSON.stringify(item._id)})'><i class="fa fa-trash-o"></i></button>
+        <button onclick='onclickEdit(${JSON.stringify(item._id)})'><i class="fa fa-edit"></i></button>
+    </li>`
+  });
+}
+
+async function onSubmit() {
+  event.preventDefault();
+  const nomeEl = document.getElementById("itemName");
+  const precoEl = document.getElementById("itemPrice");
+  const nome = nomeEl.value;
+  const preco = precoEl.value;
+
+  if (nome === "") return;
+  if (preco === "") return;
+
+  const item = { name: nome, price: preco };
+
+  await saveRemote(item);
+  const list = await getAll();
+  saveLocal(list)
+  createItemsList(list);
+
+  nomeEl.value = "";
+  precoEl.value = "";
+}
+
+window.onload = async () => {
+  let list = ''
+
+  if (getLocal().length !== 0) {
+    console.log(getLocal())
+    list = getLocal()
+  } else {
+    list = await getAll(); 
+  }
+  createItemsList(list);
+};
